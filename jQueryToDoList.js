52c1e47b5db94ed93dbc924632ad1f97c5d6c8ad
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-12-17 07:20:57 -0800

    Update jQueryToDoList.js

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -2,37 +2,39 @@
  * Toggles "done" class on <li> element
  */
 const toggle = function () {
-  console.log(this);
   $(this).toggleClass("done");
+  $("ul").on("click", "li", toggle);
 };
-$("ul").on("click", "li", toggle);
 /**
  * Delete element when delete link clicked
  */
 
 const deleteParent = function () {
   $(this).parent().remove(); // remove element which is being clicked
+
+  $("a.delete").on("click", deleteParent);
 };
-$("a.delete").on("click", deleteParent);
 /**
  * Adds new list item to <ul>
  */
+
 const addListItem = function (e) {
   e.preventDefault();
   const text = $(this).parent().find("input").val();
   $(this).parent().find("input").val("");
 
   // rest here...
-  const li = $(`<li>`);
-  const span = document.querySelector("#new-todo") + `${text}`;
-  li.append(span);
+  const $li = $("<li>");
+  const $span = $(`<span>${text}</span>`);
+  $li.append($span);
 
-  const deleteBtn = document.querySelector(".delete");
-  deleteBtn.click(deleteParent);
-  li.append(deleteBtn);
+  const $deleteBtn = $(`<a class="delete">Delete</a>`);
+  $deleteBtn.click(deleteParent);
+  $li.append($deleteBtn);
 
-  const ul = document.querySelector(`.today-list`);
-  ul.append(li);
+  const $ul = $("ul");
+  $ul.append($li);
+
+  // add listener for add
+  $(".add-item").click(addListItem);
 };
-// add listener for add
-$(`.add-item`).click(addListItem);
